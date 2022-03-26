771e0488aa5a6032c17788fbca74908dc7e2feb7
weijingbo <921716872@qq.com>
2022-03-26 15:23:38 +0800

    3.26更新

---
+++ src/main/java/LeetCode/March/TrappingRainWater.java
@@ -0,0 +1,17 @@
+package LeetCode.March;
+
+import java.util.ArrayDeque;
+
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        int res = 0;
+        for(int i = 0;i<height.length;i++){
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
